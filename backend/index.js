1e8ef5e6e3b861c44429ad4d9f9c493424c25ca1
Abhinav Raushan <shreyashabhisri@gmail.com>
2026-04-24 17:25:43 +0530

    Release #3

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,16 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://abhinavrishi45.github.io",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // // Routes (to be imported later)
